e6f4461e0b5b4facdf59907c80f196094e94111c
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 17:52:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,14 @@
      <div class="border">
      <?php
      $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
-     $id = $data->getElementsByTagName("id")->item(0)->nodeValue;
      $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+     $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+     $album = $data->getElementsByTagName("album")->item(0)->nodeValue;
 
     echo $title;
-    echo $id;
     echo $link;
+    echo $description;
+    echo $album;
     ?>
      </div>
   <?php
