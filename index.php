04811331da8e877d0d62ca1d682aad97e58dab8c
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 17:33:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  $domOBJ = new DOMDocument();
  $domOBJ->load("https://kitarvin23.herokuapp.com/rss.php");//XML page URL
 
- $content = $domOBJ->getElementsByTagName("track");
+ $content = $domOBJ->getElementsByTagName("item");
 ?>
 
  <h1>Tracks</h1>
