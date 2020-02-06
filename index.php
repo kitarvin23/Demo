e9649c9aa4f9fe63462e5759ef4fe3f89dc0cd3c
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 18:02:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,12 @@
    $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
    $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
    $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
-   $album = $data->getElementsByTagName("album")->item(0)->nodeValue;
   
     echo "
             <li>$title
             <ul>
                 <li>$link</li>
                 <li>$description</li>
-                <li>$album</li>
             </ul>
             </li>
          ";
