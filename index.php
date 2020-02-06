b3dae84aaad18486df7739012bf6740d97a805ae
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 17:54:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
+//index.php//github
 <?php
  $domOBJ = new DOMDocument();
  $domOBJ->load("https://kitarvin23.herokuapp.com/rss.php");//XML page URL
-
- $content = $domOBJ->getElementsByTagName("item");
-?>
-
- <h1>Songs</h1>
-
- <?php
+ 
+ $content = $domOBJ->getElementsByTagName("book");
+ 
+ ?>
+ <ul>
+    <?php
  foreach( $content as $data )
- {?>
-     <div class="border">
-     <?php
-     $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
-     $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
-     $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
-     $album = $data->getElementsByTagName("album")->item(0)->nodeValue;
-
-    echo $title;
-    echo $link;
-    echo $description;
-    echo $album;
-    ?>
-     </div>
-  <?php
+ {
+   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+   $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+   $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+   $album = $data->getElementsByTagName("album")->item(0)->nodeValue;
+  
+    echo "
+            <li>$title
+            <ul>
+                <li>$link</li>
+                <li>$description</li>
+                <li>$album</li>
+            </ul>
+            </li>
+         ";
+ 
  }
 ?>
-</div>
-</div>
+</ul>
