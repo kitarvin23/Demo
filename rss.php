890cdd254415d021b62a3c7b5111e0dab8b4583d
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 13:51:21 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -4,8 +4,8 @@ $rss= '<?xml version="1.0" encoding="UTF-8"?>';
 $rss .= '<rss version="2.0">';
 $rss .= '<channel>';
 
-$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820681") or die (mysqli_error($connect));
-$sql = "SELECT * FROM tblBook";
+$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820771") or die (mysqli_error($connect));
+$sql = "SELECT * FROM rss_info";
 $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
 
@@ -13,7 +13,7 @@ while($record= mysqli_fetch_assoc($query)) {
     extract($record);
     $rss .= '<book>';
     $rss .= '<title>' . $title . '</title>';
-    $rss .= '<author>' . $author . '</author>';
+    $rss .= '<link>' . $link . '</link>';
     $rss .= '<description>' . $description . '</description>';
     $rss .= '</book>';
 }
