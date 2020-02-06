b77260b57ea5a453733b2218a77e3a2ab0f535b1
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 17:12:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,23 +6,22 @@ $domOBJ->load("https://kitarvin23.herokuapp.com/rss.php");//XML page URL
  $content = $domOBJ->getElementsByTagName("item");
 ?>
 
- <h1>Tracks</h1>
+ <h1>Songs</h1>
 
  <?php
  foreach( $content as $data )
  {?>
      <div class="border">
      <?php
-     $id = $data->getElementsByTagName("id")->item(0)->nodeValue;
      $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
      $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
      $item = $data->getElementsByTagName("link")->item(0)->nodeValue;
      echo "<ul>
-            <h2>$id</h2>
+            <h2>ID:$id</h2>
               <ul>
-                  <li>Artist: $title </li>
-                  <li>Genre: $description </li>
-                  <li>Album: $link </li>
+                  <li>title: $title </li>
+                  <li>description: $description </li>
+                  <li>link: $link </li>
               </ul>
           </ul>";
     ?>
