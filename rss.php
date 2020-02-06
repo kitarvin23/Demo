bbce207500037c53be477e531996c178942e4ff2
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 17:21:11 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,38 +1,23 @@
-<?php
-// Create connection
-$con = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820771");	
+<?php 
 
-// Check connection
- if (mysqli_connect_errno($con)) {
- echo "Database connection failed!: " . mysqli_connect_error();
- }
- 
- $sql = "SELECT * FROM rss_info";
+$rss= '<?xml version="1.0" encoding="UTF-8"?>';
+$rss .= '<rss version="2.0">';
+$rss .= '<channel>';
 
- $query = mysqli_query($con,$sql);
+$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820771") or die (mysqli_error($connect));
+$sql = "SELECT * FROM rss_info";
+$query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
  
- header( "Content-type:");
- 
- echo "<?xml version='1.0' encoding='UTF-8'?>
- <rss version='2.0'>
- <channel>
- <title></title>
- <link></link>
- <language>en-us</language>";
- 
- while($row = mysqli_fetch_array($query)){
-   $id=$row["id"];
-   $title=$row["title"];
-   $link=$row["link"];
-   $description=$row["description"];
 
-   echo "<song>
-   <ID>$id</ID>
-   <title>$title</title>
-   <link>$link</link>
-   <Description>$description</Description>
+while($record= mysqli_fetch_assoc($query)) {
+    extract($record);
+    $rss .= '<book>';
+    $rss .= '<title>' .$title . '</title>';
+    $rss .= '<link>' . $link . '</link>';
+    $rss .= '<description>' . $description . '</description>';
+    $rss .= '</book>';
+}
+$rss .= '</channel>';
+$rss .= '</rss>';
 
-   </song>";
- }
- echo "</channel></rss>";
-?>
+echo  $rss  ; 
