83c5b5e6e0a191c1ea20464a0fac84772e24fc23
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 16:34:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,21 @@ $domOBJ->load("https://kitarvin23.herokuapp.com/rss.php");//XML page URL
     <?php
  foreach( $content as $data )
  {
+   $id = $data->getElementsByTagName("id")->item(0)->nodeValue;
    $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
    $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
-    echo '{';
-    echo $title;
-   echo ',';
-    echo $link;
-    echo '}';
+   $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+
+   echo "
+            <li>$title
+            <ul>
+                  <li>$id</li>
+                <li>$link</li>
+                <li>$description</li>
+            </ul>
+            </li>
+         ";
+ 
  
  }
 ?>
