cd880dd4cd2c89d447c3fb9befeb85d2c91b8e9d
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 17:35:48 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,25 +1,32 @@
 <?php 
+    $conn = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234") or die (mysqli_error($conn));
+    $db = mysqli_select_db($conn, "db_1820522");
 
-$rss= '<?xml version="1.0" encoding="UTF-8"?>';
-$rss .= '<rss version="2.0">';
-$rss .= '<channel>';
+    if(mysqli_connect_errno($conn)){
+        echo "Database connection failed!: ". mysqli_connect_errno();
+    }
+    $sql = "SELECT * FROM rss_info ORDER BY id DESC LIMIT 20";
+    $q = mysqli_query($conn, $sql);
 
-$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820771") or die (mysqli_error($connect));
-$sql = "SELECT * FROM rss_info";
-$query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
- 
+    header("Content-type: text/xml");
 
-while($record= mysqli_fetch_assoc($query)) {
-    extract($record);
-    $rss .= '<book>';
+    echo "<?xml version='1.0' encoding='UTF-8'?>
+        <rss version='2.0'><channel>";
     
-    $rss .= '<ID>' .$id . '</ID>';
-    $rss .= '<title>' .$title . '</title>';
-    $rss .= '<link>' . $link . '</link>';
-    $rss .= '<description>' . $description . '</description>';
-    $rss .= '</book>';
-}
-$rss .= '</channel>';
-$rss .= '</rss>';
+    while($r = mysqli_fetch_array($q)){
 
-echo  $rss  ; 
+        $title = $r['title'];
+        $description = $r['description'];
+        $link = $r['link'];
+
+        echo "<item>
+        <title>$title</title>
+        <genre>$description</genre>
+        <artist>$link</artist>
+        </item>";
+    }
+    echo "</channel></rss>";
+?>
+
+
+
