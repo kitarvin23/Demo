7ffedd2f5f92bf380bd417d0ea26248c3fc42c0b
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 17:43:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  $content = $domOBJ->getElementsByTagName("item");
 ?>
 
- <h1>Tracks</h1>
+ <h1>Songs</h1>
 
  <?php
  foreach( $content as $data )
@@ -13,10 +13,11 @@
      <div class="border">
      <?php
      $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
-     $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+     $id = $data->getElementsByTagName("id")->item(0)->nodeValue;
      $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+
     echo $title;
-    echo $description;
+    echo $id;
     echo $link;
     ?>
      </div>
