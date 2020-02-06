36f6ecfe559f96e36c7b0068c1e3dad096a12864
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 14:51:06 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -7,18 +7,24 @@ $rss .= '<channel>';
 $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820771") or die (mysqli_error($connect));
 $sql = "SELECT * FROM rss_info";
 $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
-
-
-while($record= mysqli_fetch_assoc($query)) {
-    extract($record);
-    $rss .= '<book>';
-    $rss .= '<title>' .$title . '</title>';
-    $rss .= '<link>' . $link . '</link>';
-    $rss .= '<description>' . $description . '</description>';
-    $rss .= '</book>';
-}
-$rss .= '</channel>';
-$rss .= '</rss>';
-
-echo  $rss  ; 
+ 
+ $content = $domOBJ->getElementsByTagName("item");
+ 
+ ?>
+ <ul>
+    <?php
+ foreach( $content as $data )
+ {
+   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+   $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+  
+    echo "{<li>$title
+            <ul>
+                <li>$link</li>
+            </ul>
+        </li>}";
+ 
+ }
 ?>
+</ul>
+
