1b24d0cec63b1f0def48e39db7154c9cf3f81290
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-02-06 18:54:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
  $domOBJ = new DOMDocument();
  $domOBJ->load("https://kitarvin23.herokuapp.com/rss.php");//XML page URL
  
- $content = $domOBJ->getElementsByTagName("track");
+ $content = $domOBJ->getElementsByTagName("song");
 ?>
 
- <h1>Tracks</h1>
+ <h1>Songs</h1>
 
  <?php
  foreach( $content as $data )
